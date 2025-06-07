ecd6f77f5d951671960c195ed826a30d0a1647a0
Emmy <example@gmail.com>
2025-06-08 00:18:43 +0200

    debugging

--- work1/question2.js
+++ work1/question2.js
@@ -1,9 +1,6 @@
 const compute= arr => {
-  if(!arr){
-    return [];
-  }
   positives=arr.filter(num=>num>0);
-  negatives=arr.filter(num => num<0)
-  return[positives.length,negatives.reduce((a,b)=>a+b,0)]
+  negatives=arr.filter(num => num<0);
+  return !positives.length && !negatives.length?[]:[positives.length ,negatives.reduce((a,b)=>a+b,0)]
 }
-console.log(compute([1,6,2,-7,-2,0,2]))
+console.log(compute([9,2,-3,-2,4,0,2,0,-7]))
